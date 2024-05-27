5c954c6dc5a9ae759d61fb77d32442fd7609bff3
yosyz <szymone25@gmail.com>
2024-05-27 18:22:41 +0200

    optimalization

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -24,28 +24,27 @@ def evaluate(G, genome, start, finish):
             return np.inf
         
         for next_node in genome:
-            if next_node in neighbors and not visited[next_node]:
+            if next_node in neighbors:
                 path_length += G.edges[current_node, next_node]['weight']
                 current_node = next_node
                 visited[next_node] = True
                 break
-        else:
-            return np.inf
-    
+            
     return path_length
 
-# Selection function - we will use the roulette wheel selection
-def selection(genomes, fitness_values, generation_size, num_chosen):
+def calculate_roulette_weights(fitness_values):
     path_lengths_inverted = np.where(fitness_values != 0, 1 / np.array(fitness_values), 0)
     min_nonzero = np.min(path_lengths_inverted[path_lengths_inverted != 0])
     roulette_weights = np.where(path_lengths_inverted != 0, np.sqrt(path_lengths_inverted), np.sqrt(min_nonzero / 2))
     norm_roulette_weights = roulette_weights / np.sum(roulette_weights)
-    
+    return norm_roulette_weights
+
+# Selection function - we will use the roulette wheel selection
+def selection(genomes, generation_size, num_chosen, norm_roulette_weights):
     selected_genomes = []
     selected_indices = np.random.choice(range(generation_size), size=num_chosen, replace=False, p=norm_roulette_weights)
-    selected_genomes.append(genomes[selected_indices[0]])
-    selected_genomes.append(genomes[selected_indices[1]])
-    
+    for idx in selected_indices:
+        selected_genomes.append(genomes[idx])
     return selected_genomes
     
 
@@ -53,7 +52,8 @@ def selection(genomes, fitness_values, generation_size, num_chosen):
 # 1. We will keep first k elements from the first parent.
 # 2. We will then add the remaining elements from the other parent in the order they appear. We will not add duplicates.
 def crossover(parent1, parent2):
-    cutoff = np.random.randint(1, len(parent1))
+    # We want to preserve at least a part of each parent
+    cutoff = np.random.randint(1, len(parent1) - 1)
     cut = parent1[:cutoff]
     child_genome = cut + [x for x in parent2 if x not in cut]
     return child_genome
@@ -63,6 +63,6 @@ def crossover(parent1, parent2):
 def mutate(genome, rate=0.1):
     for i in range(len(genome)):
         if np.random.random() <= rate:
-            rand_idx = np.random.randint(0, len(genome) - 1)
+            rand_idx = np.random.randint(0, len(genome))
             genome[i], genome[rand_idx] = genome[rand_idx], genome[i]
     return genome
