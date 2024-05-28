a88e9162b1686d932c0213a7aae5f037c006ba54
yosyz <szymone25@gmail.com>
2024-05-28 16:58:50 +0200

    created animations

--- main.py
+++ main.py
@@ -4,18 +4,30 @@ from genetic_algorithm import initialize_population, evaluate, selection, crosso
 from utils import draw_graph, create_complete_graph
 import sys
 import random
+import imageio.v2 as imageio
+
+def create_graph():
+    G = nx.Graph()
+    edges = [
+        (0, 1, 4), (0, 7, 8), (1, 2, 8), (1, 7, 11), (2, 3, 7), (2, 8, 2),
+        (2, 5, 4), (3, 4, 9), (3, 5, 14), (4, 5, 10), (5, 6, 2), (6, 7, 1),
+        (6, 8, 6), (7, 8, 7)
+    ]
+    G.add_weighted_edges_from(edges)
+    return G
 
 def main():
-    print(1 / np.inf)
+    # print(1 / np.inf)
     np.random.seed(5)
     
-    vertex_count = 46
+    # vertex_count = 46
     
     # G = nx.tutte_graph()
     # # Assign random weights to the edges
     # for (u, v) in G.edges():
     #     G.edges[u, v]['weight'] = np.random.randint(1, 50)
-    G = create_complete_graph(vertex_count)
+    G = create_graph()
+    pos = nx.spring_layout(G, seed=1)
 
 
     # pos = nx.spring_layout(G, seed=1)
@@ -23,23 +35,26 @@ def main():
 
     np.random.seed()
 
-    start = 31
-    finish = 36
+    start = 0
+    finish = 4
     path_length = nx.dijkstra_path_length(G, source=start, target=finish)
     print("Dijkstra result: " + str(path_length))
 
-    generation_size = 20
+    generation_size = 5
     elite_size = 1
     num_nodes = len(G.nodes)
     population = initialize_population(generation_size, start, num_nodes)
 
     best_path_length = sys.maxsize
     best_genome = []
+    iteration_count = 0
     no_improvement = 0
     max_no_improvement = 20
+    images = []
     
     
     while no_improvement < max_no_improvement:
+        iteration_count += 1
         fitness_values = [evaluate(G, genome, start, finish) for genome in population]
         print(fitness_values)
         print(min(fitness_values))
@@ -51,6 +66,9 @@ def main():
             no_improvement = 0
         else:
             no_improvement += 1
+            
+        draw_graph(G, pos, genome=best_genome, start=start, finish=finish, iteration=iteration_count, mutation_rate=0.1)
+        images.append(imageio.imread(f"frame_{iteration_count}.png"))
 
         elite_indices = np.argsort(fitness_values)[:elite_size]
         elite_genomes = [population[idx] for idx in elite_indices]
@@ -75,6 +93,11 @@ def main():
 
     print("Best path length found: " + str(best_path_length))
     print("Best genome: " + str(best_genome))
+    
+    if images:
+        imageio.mimwrite('genetic_algorithm.gif', images, duration=5)
+    else:
+        print("No images to create a GIF.")
 
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -1,17 +1,50 @@
 
 import networkx as nx
 import matplotlib
-matplotlib.use('Qt5Agg')
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 
-def draw_graph(G, pos):
-    nx.draw_networkx_nodes(G, pos, node_size=20)
-    nx.draw_networkx_edges(G, pos, width=2)
-    nx.draw_networkx_labels(G, pos, font_size=7, font_family="sans-serif")
-    edge_labels = nx.get_edge_attributes(G, "weight")
-    nx.draw_networkx_edge_labels(G, pos, edge_labels)
-    plt.show()
+
+
+def draw_graph(G, pos, genome=None, start=None, finish=None, iteration=None, mutation_rate=None):
+    plt.clf()
+    plt.title(f"Generation {iteration}")
+    nx.draw(G, pos, with_labels=True, node_color='lightpink', node_size=700, font_weight='bold')
+    labels = nx.get_edge_attributes(G, 'weight')
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
+    
+    draw_red_edges = True
+    
+    if genome is not None and start is not None and finish is not None:
+        current_node = start
+        best_path = [start]
+        visited = [False] * len(G.nodes)
+        visited[start] = True
+    
+        while current_node != finish:
+            neighbors = [neighbor for neighbor in G.neighbors(current_node) 
+                        if not visited[neighbor]]
+            if not neighbors:
+                draw_red_edges = False
+                break
+        
+            for next_node in genome:
+                if next_node in neighbors:
+                    current_node = next_node
+                    best_path.append(next_node)
+                    visited[next_node] = True
+                    break
+        
+        if draw_red_edges and best_path[-1] != finish:
+            best_path.append(finish)
+            
+        if draw_red_edges:
+            edges = [(best_path[i], best_path[i + 1]) for i in range(len(best_path) - 1)]
+            nx.draw_networkx_edges(G, pos, edgelist=edges, edge_color='r', width=2)
+
+    plt.savefig(f"frame_{iteration}.png")
+    
 
 def create_complete_graph(vertex_count):
     G = nx.complete_graph(vertex_count)
